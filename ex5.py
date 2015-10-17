cef4280105e6acd501da59f35afc964ab9ab1d5f
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-17 23:50:09 +0700

    Convert lbs to pounds and inches to cm in ex5.py

--- ex5.py
+++ ex5.py
@@ -7,8 +7,8 @@ teeth = 'White'
 hair = 'Brown'
 
 print "Let's talk about %s." % name
-print "He's %d inches tall." % height
-print "He's %d pounds heavy." % weight
+print "He's %d cm tall." % (height * 2.54)
+print "He's %d kg heavy." % (weight * 0.453592)
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
