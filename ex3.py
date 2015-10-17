3ca55fb02c6dee0ba6e1c16262fb322153c14841
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-17 21:04:28 +0700

    Add comments to ex3.py

--- ex3.py
+++ ex3.py
@@ -1,23 +1,37 @@
+# prints the string "I will now count my chickens:"
 print "I will now count my chickens:"
 
+# prints the string "Hens" and the result of the operation 25 plus (30 devided by 6)
 print "Hens", 25 + 30 / 6
+# prints the string "Roosters" and the result of the operation 100 minus the modulo of ((25 times 3) devided 4)
 print "Roosters", 100 - 25 * 3 % 4
 
+# prints the string "Now I will count the eggs:"
 print "Now I will count the eggs:"
 
+# prints the result of the operation 3 plus 2 plus 1 minus 5 plus modulo of (4 devided by 2) (which is 0) minus integer of (1 devided by 4) (which is 0) plus 6
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# prints the string "Is it true that 3 + 2 < 5 - 7?"
 print "Is it true that 3 + 2 < 5 - 7?"
 
+# prints the evaluation (True or False) of the statment that 3 + 2 is less than 5 - 7 (False)
 print 3 + 2 < 5 - 7
 
+# prints the string "What is 3 + 2?" and the result of the operation 3 plus 2
 print "What is 3 + 2?", 3 + 2
+# prints the sting "What is 5 - 7?" and the result of the operation 5 minus 7
 print "What is 5 - 7?", 5 - 7
 
+# prints the string "Oh, that's why it's False."
 print "Oh, that's why it's False."
 
+# prints the string "How about some more."
 print "How about some more."
 
+# prints the string "Is it greater?" and the result of the evaluation of the statment that 5 is greater than -2 (True)
 print "Is it greater?", 5 > -2
+# prints the sting "Is it greater or equal?" and the result of the evaluation of the statment that 5 is greater or equal to -2 (True)
 print "Is it greater or equal?", 5 >= -2
+# prints the string "Is it less or equal?" and the result of the evaluation of the statment that 5 is less or equal to -2 (False)
 print "Is it less or equal?", 5 <= -2
