d648b74678aa0c855912794d597509020bf4f959
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-18 16:38:06 +0700

    Add comments to ex7.py

--- ex7.py
+++ ex7.py
@@ -1,21 +1,39 @@
+# print the string
 print "Mary had a little lamb."
+# print the string and insert 'snow' in place of %s as a string
 print "Its fleece was white as %s." % 'snow'
+# print the string
 print "And everywhere that Mary went."
+# print the string "." ten times
 print "." * 10
 
+# assign the value of the variable end1 to the string "C"
 end1 = "C"
+# assign the value of the variable end2 to the string "h"
 end2 = "h"
+# assign the value of the variable end3 to the string "e"
 end3 = "e"
+# assign the value of the variable end4 to the string "e"
 end4 = "e"
+# assign the value of the variable end5 to the string "s"
 end5 = "s"
+# assign the value of the variable end6 to the string "e"
 end6 = "e"
+# assign the value of the variable end7 to the string "B"
 end7 = "B"
+# assign the value of the variable end8 to the string "u"
 end8 = "u"
+# assign the value of the variable end9 to the string "r"
 end9 = "r"
+# assign the value of the variable end10 to the string "g"
 end10 = "g"
+# assign the value of the variable end11 to the string "e"
 end11 = "e"
+# assign the value of the variable end12 to the string "r"
 end12 = "r"
 
 # watch that comma at the end. try removing it to see what happens
+# print the values of the variables end1, end2, end3, end4, end5, end6 one after another
 print end1 + end2 + end3 +  end4 +  end5 +  end6,
+# print the values of the variables end7, end8, end9, end10, end11, end12 one after another
 print end7 +  end8 +  end9 +  end10 +  end11 +  end12
