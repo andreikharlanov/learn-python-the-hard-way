46c1e4b0053684d14128747d42465265d7ac0706
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-19 12:31:03 +0700

    Compare %s and %r string formats with escapes

---
+++ ex10_r_string_format.py
@@ -0,0 +1,5 @@
+take_4_strings_with_r = "%r, %r, %r, %r"
+take_4_strings_with_s = "%s, %s, %s, %s"
+
+print "This is r format: ", take_4_strings_with_r % ("\" double","\' single","\" double","\' single")
+print "This is s format: ", take_4_strings_with_s % ("\" double","\' single","\" double","\' single")
