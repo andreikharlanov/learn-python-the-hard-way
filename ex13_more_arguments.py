61215a1bf4691e7f7a8c9f820767762020ab3a03
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-19 19:24:58 +0700

    Make a script with more arguments for the Ex 13

---
+++ ex13_more_arguments.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second, third, fourth = argv
+
+print "So, you called", script, "script"
+print "And gave it this argument:", first
+
+# and I don't want to print more arguments
