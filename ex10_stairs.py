112cefe8ae3408270df1d7475d28f3368fed648d
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-19 12:16:39 +0700

    Combine escapes and string formatting

---
+++ ex10_stairs.py
@@ -0,0 +1,19 @@
+format_me = "*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s*%s"
+
+print format_me % (
+    "One\n",
+    "One\t",
+    "Two\n",
+    "One\t",
+    "Two\t",
+    "Three\n",
+    "One\t",
+    "Two\t",
+    "Three\t",
+    "Four\n",
+    "One\t",
+    "Two\t",
+    "Three\t",
+    "Four\t",
+    "Five\n"
+)
