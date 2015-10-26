7a3779964e7de27b049d4ae75f40f96d11ac9cdb
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-10-26 16:47:21 +0700

    Add some interesting code for Ex 23

---
+++ ex23.py
@@ -0,0 +1,32 @@
+# https://github.com/dib-lab/khmer/blob/master/khmer/thread_utils.py
+try:
+    import queue
+exept ImportError:
+    import Queue as queue
+
+## enumerate(), file=sys.stderr, yield
+for n, record in enumerate(screed_iter)
+    if n % 100000 == 0:
+        print('... filtering', n, file=sys.stderr)
+    yield record
+
+## class, __init__, self.order, self.seqlist
+class SequenceGroup(object):
+
+    def __init__(self, order, seqlist):
+        self.order = order
+        self.seqlist = seqlist
+
+# https://github.com/frombeck/COUNTING-VOWELS/blob/master/aset.py
+## filter(), lambda ch:ch.lower(), in
+s = 'azcbobobegghakl'
+print "Number of vowels:", len(filter(lambda ch:ch.lower() in "aeiou"))
+
+# https://github.com/geduldig/TwitterCounter/blob/master/TwitterCounter/Tokenizer.py
+@classmethod
+tweet = cls._remove_urls(tweet) #class?
+
+# https://github.com/millertime-homework/cs410-comba
+# https://github.com/stephenmcd/django-postgres-fuzzycount
+# https://github.com/geduldig/TwitterCounter
+# https://github.com/KeypadSDM/SenateCounting
