3ee89c6903bd1cd78ada88698de45f8cef5bf34b
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-14 00:53:35 +0700

    Add increment value to a function

--- ex33.py
+++ ex33.py
@@ -1,16 +1,16 @@
 numbers = []
 
-def print_list_creation(stop_number):
+def print_list_creation(stop_number, increment):
     i = 0
     while i < stop_number:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
-print_list_creation(6)
+print_list_creation(100,9)
 
 
 print "The numbers: "
