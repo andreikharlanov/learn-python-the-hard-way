1db9520946e5a9cf9dd503312d91e12e6d02c3bc
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-14 00:46:56 +0700

    Convert while-loop to a function in Ex 33

--- ex33.py
+++ ex33.py
@@ -1,13 +1,17 @@
-i = 0
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def print_list_creation(stop_number):
+    i = 0
+    while i < stop_number:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+print_list_creation(6)
+
 
 print "The numbers: "
 
