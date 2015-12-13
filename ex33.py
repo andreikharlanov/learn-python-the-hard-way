93d43d4c3fc803d14e16f5949ed391e765622490
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-14 01:01:35 +0700

    Rewrite Ex 33 to use for-loop and range()

--- ex33.py
+++ ex33.py
@@ -1,17 +1,14 @@
 numbers = []
 
-def print_list_creation(stop_number, increment):
-    i = 0
-    while i < stop_number:
+def print_list_creation(stop_number, step):
+    for i in range(0, stop_number, step):
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + increment
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
-print_list_creation(100,9)
-
+print_list_creation(122,13)
 
 print "The numbers: "
 
