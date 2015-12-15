0215ad98318e1c1c288dd674e12b9e55d4d4adb5
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-16 01:26:34 +0700

    Add a test file for Ex 35
    I used that filed to test a new code to test if a user's input is number.

---
+++ ex35_test.py
@@ -0,0 +1,42 @@
+from sys import exit
+
+def interpret_string(s):
+    if not isinstance(s, basestring):
+        return False
+    if s.isdigit():
+        return int(s)
+    try:
+        return float(s)
+    except ValueError:
+        return False
+
+def get_number():
+    choice = raw_input("> ")
+
+    number = interpret_string(choice)
+
+    if number == False:
+        print "Type a number, idiot!"
+        return get_number()
+    else:
+        return number
+
+def gold_room():
+    print "This room is full of gold. How much do you take?"
+
+    how_much = get_number()
+
+    if how_much < 50:
+        print "You took %r money." % how_much
+        print "Nice, you're not greedy, you win!"
+        exit(0)
+    else:
+        print "You took %r money." % how_much
+        dead("You greedy bastard!")
+
+
+def dead(why):
+    print why, "Good job!"
+    exit(0)
+
+gold_room()
