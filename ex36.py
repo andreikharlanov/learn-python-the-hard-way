ff5aafad546494e20e6ae444b41bc267bf9d3f94
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-17 14:41:42 +0700

    Add a function to go a previous room

--- ex36.py
+++ ex36.py
@@ -1,14 +1,45 @@
 steps = 0
 
+previous_room = 1
+
 text_on_the_wall = "\nM... mmm..\n"
 
 def welcome_text_room(room_number):
     print "\n----\n"
     print "You've made %d steps already.\n" % steps
-    print "You are in a room #%d\n" % room_number
+    print "You are in a room #%d.\n" % room_number
+
+def go_to_room(room_number):
+    if room_number == 1:
+        room_1()
+    elif room_number == 2:
+        room_2()
+    elif room_number == 3:
+        room_3()
+    elif room_number == 4:
+        room_4()
+    elif room_number == 5:
+        room_5()
+    elif room_number == 6:
+        room_6()
+    elif room_number == 7:
+        room_7()
+    elif room_number == 8:
+        room_8()
+    elif room_number == 9:
+        room_9()
+    elif room_number == 10:
+        room_10()
+    elif room_number == 11:
+        room_11()
+    elif room_number == 12:
+        room_12()
 
 def room_1():
     welcome_text_room(1)
+
+    global previous_room
+    previous_room = 1
 
     print "It has just two doors:"
     print "1. The first one is behind you, you just entered through it."
@@ -55,29 +86,37 @@ def room_2():
 
     def room_2_choice():
         global steps
+        global previous_room
 
         choice = raw_input("> ")
 
         if choice == "first" or choice == "1" or choice == "behind":
             print "You chose door #1. OK!"
             steps += 1
+            previous_room = 2
             room_1()
         elif choice == "second" or choice == "2" or choice == "left":
             print "You chose door #2. OK!"
             steps += 1
+            previous_room = 2
             room_3()
         elif choice == "third" or choice == "3" or choice == "small":
             print "You chose door #3. OK!"
             steps += 1
+            previous_room = 2
             room_4()
         elif choice == "fourth" or choice == "4" or choice == "infront":
-            print "You chose door #3. OK!"
+            print "You chose door #4. OK!"
             steps += 1
+            previous_room = 2
             room_5()
         elif choice == "fifth" or choice == "5" or choice == "right":
-            print "You chose door #3. OK!"
+            print "You chose door #5. You will go to the room #6."
             steps += 1
+            previous_room = 2
             room_6()
+        elif choice == "go back":
+            go_to_room(previous_room)
         else:
             print "I don't understand you."
             room_2_choice()
@@ -86,25 +125,29 @@ def room_2():
 def room_3():
     welcome_text_room(3)
 
-    print "This room is unfinished. We transfer you to a finished one."
-    room_2()
+    print "This room is unfinished. We transfer you to room you entred this one from."
+    print "Now you'll go back to %d room." % previous_room
+    go_to_room(previous_room)
 
 def room_4():
     welcome_text_room(4)
 
-    print "This room is unfinished. We transfer you to a finished one."
-    room_2()
+    print "This room is unfinished. We transfer you to room you entred this one from."
+    print "Now you'll go back to %d room." % previous_room
+    go_to_room(previous_room)
 
 def room_5():
     welcome_text_room(5)
 
-    print "This room is unfinished. We transfer you to a finished one."
-    room_2()
+    print "This room is unfinished. We transfer you to room you entred this one from."
+    print "Now you'll go back to %d room." % previous_room
+    go_to_room(previous_room)
 
 def room_6():
     welcome_text_room(6)
 
-    print "This room is unfinished. We transfer you to a finished one."
-    room_2()
+    print "This room is unfinished. We transfer you to room you entred this one from."
+    print "Now you'll go back to %d room." % previous_room
+    go_to_room(previous_room)
 
 room_1()
