84de50c04e960e3259583efbcfac94f00a2130c6
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2015-12-26 22:40:54 +0700

    Change names of the doors from left/right to color
    Names based on the location (right/left...) is wrong,
    because a player can enter a room from different doors,
    and the location (right/left...) is not universal.
    
    Colored doors are universal.
    
    The doors have the same color on both sides.

--- ex36.py
+++ ex36.py
@@ -53,30 +53,30 @@ def room_1():
     previous_room = 1
 
     print "It has just two doors:"
-    print "1. The first one is behind you, you just entered through it."
-    print "2. The second door is infront of you.\n"
+    print "1. Green door."
+    print "2. Red door.\n"
 
     print "Also there is a text on the wall. Do you want to read it?\n"
-    print "Say 'yes', if you want to read the text."
-    print "Say 'no', if you don't want to read the text and enter the door infront of you."
-    print "Say 'back', if you want to go back."
+    print "Say 'text', if you want to read the text on the wall."
+    print "Say 'red', if you want to enter the red door."
+    print "Say 'green', if you want to enter the red door."
 
     def room_1_choice():
         global steps
 
         choice = raw_input("> ")
 
-        if choice == "yes":
+        if choice == "text":
             print "Here is the text on the wall:"
             print text_on_the_wall
             steps += 1
             room_1()
-        elif choice == "no":
-            print "You open the door infront of you "
+        elif choice == "1" or choice == "red":
+            print "You open the red door infront of you."
             steps += 1
             room_2()
-        elif choice == "back":
-            print "You try to open the door behind you. Tough luck, it's closed."
+        elif choice == "2" or choice == "green" or choice == "back":
+            print "You try to open the green door. Tough luck, it's closed."
             steps += 1
             room_1()
         else:
@@ -88,11 +88,11 @@ def room_2():
     welcome_text_room(2)
 
     print "It has five doors:"
-    print "1. The first one is behind you, you just untered through it."
-    print "2. The door on your left."
-    print "3. The small door infront of you. It seems unusual."
-    print "4. The big door infront of you."
-    print "5. The door on the right.\n"
+    print "1. Red."
+    print "2. Yellow."
+    print "3. The small orange door. It seems unusual."
+    print "4. Green."
+    print "5. Blue.\n"
     print "Which one do you take?"
 
     def room_2_choice():
@@ -101,27 +101,27 @@ def room_2():
 
         choice = raw_input("> ")
 
-        if choice == "first" or choice == "1" or choice == "behind":
+        if choice == "1" or choice == "red":
             print "You chose door #1. OK!"
             steps += 1
             previous_room = 2
             room_1()
-        elif choice == "second" or choice == "2" or choice == "left":
+        elif choice == "2" or choice == "yellow":
             print "You chose door #2. OK!"
             steps += 1
             previous_room = 2
             room_3()
-        elif choice == "third" or choice == "3" or choice == "small":
+        elif choice == "3" or choice == "orange":
             print "You chose door #3. OK!"
             steps += 1
             previous_room = 2
             room_4()
-        elif choice == "fourth" or choice == "4" or choice == "infront":
+        elif choice == "4" or choice == "green":
             print "You chose door #4. OK!"
             steps += 1
             previous_room = 2
             room_5()
-        elif choice == "fifth" or choice == "5" or choice == "right":
+        elif choice == "5" or choice == "blue":
             print "You chose door #5. You will go to the room #6."
             steps += 1
             previous_room = 2
