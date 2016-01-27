a546d25277d5bf88c4fced68713181c9ea4a6b85
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-01-27 13:38:41 +0700

    Add 'and', 'break', 'continue' and 'def' to Ex 37

--- ex37.py
+++ ex37.py
@@ -1,3 +1,35 @@
+variable_1 = 1
+variable_2 = 2
+
+# and
+if variable_1 == 1 and variable_2 == 2:
+    print "Everything is OK!\n"
+
+# assert
 a = "a"
 
 assert a == "a", "Oops, 'a' is not an 'a'"
+
+# break
+for n in range(2, 7):
+    for x in range(2, n):
+        if n % x == 0:
+            print n, 'equals', x, '*', n/x
+            break
+    else:
+        # loop fell through without finding a factor
+        print n, 'is a prime number'
+
+# continue
+for i in range(1,5):
+    # if i + 1 > 2 start a new iteration of the for-loop
+    # i will be printed only if i == 1 or i == 2
+    if i + 1 > 3:
+        continue
+    print i
+
+# def
+def print_yo(name):
+    print "Yo,", name
+
+print_yo("Andrei")
