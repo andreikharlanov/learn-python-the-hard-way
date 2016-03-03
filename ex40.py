909636e991359cd55417993add1e8e91494d2c9f
Andrei Kharlanov <andrei.kharlanov@gmail.com>
2016-03-03 23:05:01 +0700

    Add a song to Ex 40 with a lyrics in a variable

--- ex40.py
+++ ex40.py
@@ -17,8 +17,16 @@ bulls_on_parade = Song(["They rally around tha family",
 ya_ya_moo = Song(["I'm the moo,",
                   "I'd like to moo."])
 
+hit_song_lyrics = ["Come here and dance with me,", "I'm the queen of this town,",
+            "Is it so hard to understand?", "Don't fool around,",
+            "Don't fool around."]
+
+hit_song = Song(hit_song_lyrics)
+
 happy_bday.sing_me_a_song()
 
 bulls_on_parade.sing_me_a_song()
 
 ya_ya_moo.sing_me_a_song()
+
+hit_song.sing_me_a_song()
